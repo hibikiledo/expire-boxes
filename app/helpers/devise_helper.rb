c5241a1288130394b25c384a5a0b401b903eeb9c
@hbk <nut.rainbowpuke@gmail.com>
2016-01-05 21:29:29 +0700

    Update flash shared template to notice

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,6 +1,5 @@
 module DeviseHelper
   def devise_error_messages!
-    return resource.errors.full_messages
     return "" if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
