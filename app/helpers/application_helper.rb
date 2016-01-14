8b6a63f74f967e986b4bca396d2b79cde808e3a1
@hbk <nut.rainbowpuke@gmail.com>
2016-01-14 11:24:02 +0700

    Always show loading animation until ajax response is received

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,11 @@ module ApplicationHelper
       <div id="#{id}" class="modal fade">
         #{render 'dashboard/shared/loading_dialog'}
       </div>
+      <script>
+      $("##{id}").on("show.bs.modal", function (e) {
+        $("##{id}").html('#{escape_javascript(render 'dashboard/shared/loading_dialog')}');
+      });
+      </script>
     HTML
     html.html_safe
   end
